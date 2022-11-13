63753d77371762372ea53d28efc449c422adfded
Aksa George <aksajeeku@gmail.com>
2022-11-13 13:53:31 -0500

    Quotation Save Completed

--- server/routes/api/quoations.js
+++ server/routes/api/quoations.js
@@ -3,32 +3,49 @@ const express = require("express");
 const Quotations = require("../../models/Quotations");
 const router = express.Router();
 
-router.get('/customization', async(req,res) => {
-  Quotations.findOne().sort({'_id':-1}).limit(1)
-          .then((data) => {
-            res.send(data);
-          })
-          .catch((err) => {
-            res.status(404).send({
-              message: err.message || "Error while retrieving quote",
-            });
-          });
-  })
+router.get('/customization', async (req, res) => {
+  Quotations.findOne().sort({ '_id': -1 }).limit(1)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(404).send({
+        message: err.message || "Error while retrieving quote",
+      });
+    });
+})
 
-router.post('/customization', async(req,res) => {
+router.post('/customization', async (req, res) => {
   const newQuote = new Quotations(req.body)
-  try{
+  try {
     await newQuote.save()
     res.status(201).json({
       status: 'Success',
-      data : { newQuote}
+      data: { newQuote }
     })
-  }catch(err){
+  } catch (err) {
     res.status(500).json({
-      status:'Failed',
+      status: 'Failed',
       message: err
     })
   }
+})
+
+
+router.post('/addQuotation', (req, res, next) => {
+  let quotation = new Quotations(req.body);
+  quotation.save().then(()=>{
+    res.status(201).json({
+      status:"Success",
+      data : {quotation}
+    }).catch(err=>{
+      res.status(500).json({
+        status: 'Failed',
+        message: err
+      })
+    })
   })
+
+})
 
 module.exports = router;
