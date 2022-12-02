f30742329786c98c8d91a4912435b0b2c97065fa
kokilascse <90804368+kokilascse@users.noreply.github.com>
2022-12-02 12:55:18 -0500

    Home and Search now visible for LoggedIn customer

---
+++ client/src/components/Navbar/LoggedinMenu.js
@@ -0,0 +1,15 @@
+
+export const LoggedinMenu = [
+    {
+      title: "Home",
+      url: "/",
+      cName: "nav-link",
+    },
+    {
+      title: "Search Product",
+      url: "/search",
+      cName: "nav-link",
+    }
+    
+  ];
+  
